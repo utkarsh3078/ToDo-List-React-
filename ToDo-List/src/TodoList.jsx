aab084ff32b010ca27a79e3ecc56b2ce3437fe89
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2025-04-14 10:23:44 +0530

    Basic Structure done
    Made the page and given the id to each element we will add in todo

--- ToDo-List/src/TodoList.jsx
+++ ToDo-List/src/TodoList.jsx
@@ -1,18 +1,35 @@
-import { useState } from "react"
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList(){
 
-    let [toDo, setToDo] = useState([]);
+    let [toDo, setToDo] = useState([{task:"Sample Task" , id: uuidv4() }]);
+    let [newToDo, setNewToDo] = useState("");
+
+    let addNewTask = () =>{
+        setToDo((prevTodo) => {
+            return [...prevTodo, {task: newToDo, id: uuidv4() }];
+        });
+        setNewToDo("");  //To reseting the value of input field
+    }
+
+    let handleChange = (event) =>{
+        setNewToDo(event.target.value);
+        console.log(event.target.value);
+    }
     return(
         <div>
             <h1>ToDo List (React)</h1>
-            <input placeholder="add a task"></input>
-            <button>Add</button>
+            <input placeholder="add a task" value={newToDo} onChange={handleChange}></input>
+            <button onClick={addNewTask}>Add</button>
             <br /><br /> <br />
             <hr></hr>
             <h3>Tasks:</h3>
+            <br />
             <ul>
-                
+                {toDo.map((todo)=>(
+                    <li key={toDo.id}>{todo.task}</li>
+                ))}
             </ul>
         </div>
     )
