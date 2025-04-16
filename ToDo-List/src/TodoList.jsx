b09732ffe9dae1ace77401fe78832ca3dd4b8cbd
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2025-04-16 05:42:17 +0530

    Done with all functionalities
    I have completed the mark as done feature.
    Now I have to start working on two parts:-
    1. CSS
    2. component display acc to every device

--- ToDo-List/src/TodoList.jsx
+++ ToDo-List/src/TodoList.jsx
@@ -3,15 +3,14 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList(){
 
-    let [toDo, setToDo] = useState([{task:"Sample Task" , id: uuidv4() }]);
+    let [toDo, setToDo] = useState([{task:"Sample Task" , id: uuidv4(), isDone: false }]);
     let [newToDo, setNewToDo] = useState("");
-    let [isDone, setIsDone] = useState("false")
 
     let addNewTask = () =>{
         setToDo((prevTodo) => {
-            return [...prevTodo, {task: newToDo, id: uuidv4(), done: isDone }];
+            return [...prevTodo, {task: newToDo, id: uuidv4(), isDone: false }];
         });
-        setNewToDo("");  //To reseting the value of input field
+        setNewToDo("");  //To reseting the value of input field 
     }
 
     let handleChange = (event) =>{
@@ -24,16 +23,18 @@ export default function TodoList(){
         setToDo(copy);
     }
     
-    let allDone = () => {    
-        setToDo( toDo.map((todo)=> {
-            return {
-                ...todo,
-                task: todo.task.toUpperCase(),
-            }
-        }))
+    let allUppercase = () => {    
+        setToDo( (preValue) => 
+            preValue.map((todo) => {
+                return {
+                    ...todo,
+                    task: todo.task.toUpperCase(),
+                }
+            })
+         )
     }
 
-    let oneDone = (id) => {    
+    let oneUppercase = (id) => {    
         setToDo((prevValue) => 
             prevValue.map((todo) => {
                 if(todo.id === id){
@@ -48,6 +49,32 @@ export default function TodoList(){
         )
     }
 
+    let oneDone = (id) => {    
+        setToDo((prevValue) => 
+            prevValue.map((todo) => {
+                if(todo.id === id){
+                    return {
+                        ...todo,
+                        isDone: true,
+                    }
+                }else{
+                    return todo;
+                }
+            })
+        )
+    }
+
+    let alldone = () => {    
+        setToDo( (preValue) => 
+            preValue.map((todo) => {
+                return {
+                    ...todo,
+                    isDone: true,
+                }
+            })
+         )
+    }
+
     return(
         <div>
             <h1>ToDo List (React)</h1>
@@ -60,17 +87,20 @@ export default function TodoList(){
             <ul>
                 {toDo.map((todo)=>(
                     <li key={todo.id}>
-                        <span>{todo.task}</span> 
+                        <span style={todo.isDone ? {textDecorationLine: "line-through"} : {}}>{todo.task}</span> 
                         &nbsp; &nbsp;
                         <button onClick={() => deleteToDo(todo.id)}>Delete</button>
                         &nbsp; &nbsp;
-                        <button onClick={() => oneDone(todo.id)}>UpperCase</button>
+                        <button onClick={() => oneUppercase(todo.id)}>UpperCase</button>
+                        &nbsp; &nbsp;
+                        <button onClick={() => oneDone(todo.id)}>Mark as done</button>
                         
                     </li>
                     
                 ))}
             </ul>
-            <button onClick={allDone}>UpperCase all</button>
+            <button onClick={allUppercase}>UpperCase all</button>
+            <button onClick={alldone}>Mark as Done</button>
         </div>
     )
 }
